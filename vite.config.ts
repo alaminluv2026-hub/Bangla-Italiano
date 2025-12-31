8d344a3583fd0e85a71d2fd57faba136dedf2699
alaminluv2026-hub <alaminluv2026@gmail.com>
2025-12-31 03:00:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',       
+    port: 10000,          
+    strictPort: true,
     allowedHosts: [
       'bangla-italiano-22.onrender.com'
     ]
